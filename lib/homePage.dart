8dc649c3728670f7780baa208a489fc38dea67b4
Hankyu2000 <yuduki.developer@gmail.com>
2023-06-28 10:12:30 +0900

    Made BottomNavigationBar Operational

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -2,31 +2,46 @@ import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:nagisa_talk/chatPage.dart';
+import 'package:nagisa_talk/studentsList.dart';
 
-class HomePage extends StatelessWidget{
+class HomePage extends StatefulWidget{
   @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage>{
   int _selectedIndex = 0;
   static List<Widget> _pageList = [
     ChatPage(),
-
+    StudentsListPage()
   ];
+
+  void _onItemTapped(int index){
+    setState( (){
+      _selectedIndex = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context){
     return Scaffold(
       body: Container(
-        child: ChatPage()
+        child: _pageList[_selectedIndex]
       ),
       bottomNavigationBar: BottomNavigationBar(
         selectedItemColor: Colors.amber,
         selectedLabelStyle: TextStyle(fontWeight: FontWeight.w600),
         unselectedLabelStyle: TextStyle(fontWeight: FontWeight.w600),
         type: BottomNavigationBarType.fixed,
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
         items: [
           BottomNavigationBarItem(
             icon: Icon(Icons.chat_bubble_outline),
             label: AppLocalizations.of(context)!.bNavBarChat,
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.chat_bubble_outline),
+            icon: Icon(Icons.person_outline),
             label: AppLocalizations.of(context)!.bNavBarStudents,
           )
         ],
